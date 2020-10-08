ff8fd68012b5c73a739642474cd2c5137c662d50
Corrigan Newsome <60944330+CorriganNewsome@users.noreply.github.com>
2020-10-07 19:48:43 -0600

    Add files via upload

---
+++ Driver.cpp
@@ -0,0 +1,18 @@
+//Corrigan Newsome cnewsome2@cnm.edu
+
+//Quiz 1
+
+//Driver.cpp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "\n Corrigan Newsome, CIS Computer Programming, and to gain more programming knowledge and gain employment as a Web or Software Developer\n\n";
+	cout << "\n 1. College Algebra\n\n";
+	cout << "\n 2. I took the Full Stack Web Development Bootcamp through CNM Ingenuity\n\n";
+	cout << "\n 3. My favorite thing about programming is the problem solving aspect of it and the continuous learning involved.\n\n";
+	cout << "\n Optional: I was in the Navy for 6 and a half years, I have a Bachelor's in Business Administration, and I work full time as a Marketing Engineer.\n\n";
+	return 0;
+}
