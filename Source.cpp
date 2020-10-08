620265a4944125cc9373e4f39b556857ef8c4528
Corrigan Newsome <60944330+CorriganNewsome@users.noreply.github.com>
2020-10-07 19:46:03 -0600

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,45 @@
+//Corrigan Newsome cnewsome2@cnm.edu
+
+//Variable Demo
+
+
+//Driver.cpp
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//	cout << "\n Hello World!\n\n";
+		/*int total{5};
+		int TOTAL{10};
+		cout << "\n total: "<< total << "\n\n";
+		cout << "\n TOTAL: "<< TOTAL << "\n\n";*/
+
+		/*int myVariable{ 15 };
+		cout << "myVariable " << myVariable << "\n";*/
+
+		/*char letterOfAlphabet{ 'a' };
+		cout << "letterofAlphabet " << letterOfAlphabet << "\n";
+		int aWholeNumber{ 25 };
+		cout << "aWholeNumber " << aWholeNumber<< "\n";
+		double aReallySmallNumber{ 0.000000000 };
+		cout << "aReallySmallNumber " << aReallySmallNumber << "\n";
+		float bankBalance{ 500.00 };
+		cout << "bankBalance " << bankBalance << "\n";
+		bool bProgrammingIsFun = true;
+		cout << "bProgrammingIsFun " << bProgrammingIsFun << "\n";*/
+
+		/*int largest = { 2147483647 };
+		cout << "largest " << largest << "\n";
+		int tooLarge = {2147483648};
+		cout << "tooLarge " << tooLarge << "\n";*/
+
+	float pi = { 3.141592653589793 };
+	cout << "pi " << pi << "\n";
+
+
+
+
+
+	return 0;
+}
